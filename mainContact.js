e2f4e1b327873de24575617c66944384dc0ffc48
Martin Collahua <martin_adan2@hotmail.com>
2022-06-22 13:36:13 -0700

    ProyectoFinal

---
+++ mainContact.js
@@ -0,0 +1,72 @@
+// const form = document.querySelector("form");
+// form.addEventListener("submit",(e)=>{
+//     e.preventDefault();
+//     console.log(e);
+
+// });
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => handleSubmit(e));
+
+const validation = new window.JustValidate(form);
+
+let datosForm;
+
+
+
+validation
+	.addField("#name", [
+		{
+			rule: "required",
+			errorMessage: "Campo obligatorio",
+		},
+
+	])
+
+    .addField("#email", [
+		{
+			rule: "required",
+			errorMessage: "Campo obligatorio",
+		},
+		// {
+		// 	rule: "email",
+		// 	errorMessage: "Inserte email válido",
+		// },
+	])
+
+
+    .addField("#mensaje", [
+		{
+			rule: "required",
+			errorMessage: "Campo obligatorio",
+		},
+	
+	]);
+
+
+    validation.showSuccessLabels({ "#email": "The email looks good!" });
+
+function handleSubmit (e) {
+	e.preventDefault();
+
+    console.log(validation.isValid)
+
+    if(validation.isValid){
+        console.log("es valido");
+    }
+    else{
+        console.log("no es valido");
+    }
+
+    // if (!validation.isValid) return (console.log(validation.isValid));
+    //sino es valido devuelve el return y queda ahi
+    
+
+    const data = new FormData(form);
+	datosForm = Object.fromEntries(data.entries());
+	console.log(datosForm);
+    
+    form.reset();
+	console.log("mensaje enviado");
+
+};
